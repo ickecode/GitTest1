6c5dfff0e72a8b29f7f90a340eea435f7c4e661b
ickecode <ickecode@gmail.com>
2017-08-06 02:14:00 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello, World!");
             Console.ReadKey();
             //Comment added in GitHub
+            //Another comment added in GitHub
         }
     }
 }
